7cc72dcfab3765495e7e90bc6e1860b69d8b640e
arthur <arthureli81@gmail.com>
2026-04-14 17:46:40 -0600

    Add collected_signatures column to escrows table

---
+++ alembic/versions/20260414_1200_0005_add_escrow_signatures.py
@@ -0,0 +1,35 @@
+"""add collected_signatures to escrows
+
+Revision ID: 0005
+Revises: 0004
+Create Date: 2026-04-14 12:00:00
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+# revision identifiers, used by Alembic.
+revision: str = "0005"
+down_revision: Union[str, None] = "0004"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "escrows",
+        sa.Column(
+            "collected_signatures",
+            postgresql.JSONB(astext_type=sa.Text()),
+            nullable=True,
+            server_default=sa.text("'{}'::jsonb"),
+        ),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("escrows", "collected_signatures")
