147489c67d66ece4988021382e8355e687e6c55c
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2026-04-15 02:33:35 -0600

    feat(common): add replayable redis stream helpers

--- services/common/__init__.py
+++ services/common/__init__.py
@@ -1,6 +1,7 @@
 from .config import Settings, get_settings
 from .db import metadata
 from .events import InternalEventBus, RedisStreamMirror
+from .realtime import RedisStreamFeed, StreamEvent, decode_resume_token, encode_resume_token
 from .readiness import get_readiness_payload
 
 __all__ = [
@@ -10,4 +11,8 @@ __all__ = [
 	"metadata",
 	"InternalEventBus",
 	"RedisStreamMirror",
+	"RedisStreamFeed",
+	"StreamEvent",
+	"decode_resume_token",
+	"encode_resume_token",
 ]
